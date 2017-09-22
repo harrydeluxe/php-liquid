4163b14005954e79526b7037db9e603103205e4d
Alexey Kopytko <alexey@kopytko.com>
2017-09-22 17:42:29 +0900

    Liquid/Cache/Local - cache with data stored in an embedded variable with no handling of expiration dates for simplicity

---
+++ src/Liquid/Cache/Local.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * This file is part of the Liquid package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package Liquid
+ */
+
+namespace Liquid\Cache;
+
+use Liquid\Cache;
+
+/**
+ * Implements cache with data stored in an embedded variable with no handling of expiration dates for simplicity
+ */
+class Local extends Cache
+{
+	private $cache = array();
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function read($key, $unserialize = true) {
+		if (isset($this->cache[$key])) {
+			return $this->cache[$key];
+		}
+
+		return false;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function exists($key) {
+		return isset($this->cache[$key]);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function write($key, $value, $serialize = true) {
+		$this->cache[$key] = $value;
+		return true;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function flush($expiredOnly = false) {
+		$this->cache = array();
+		return true;
+	}
+}
---
+++ tests/Liquid/Cache/LocalTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * This file is part of the Liquid package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package Liquid
+ */
+
+namespace Liquid\Cache;
+
+use Liquid\TestCase;
+use \Liquid\Cache\Local;
+
+class LocalTest extends TestCase
+{
+	/** @var \Liquid\Cache\Local */
+	protected $cache;
+
+	protected function setUp() {
+		parent::setUp();
+
+		$this->cache = new Local();
+	}
+
+	public function testNotExists() {
+		$this->assertFalse($this->cache->exists('no_such_key'));
+	}
+
+	public function testReadNotExisting() {
+		$this->assertFalse($this->cache->read('no_such_key'));
+	}
+
+	public function testSetGetFlush() {
+		$this->assertTrue($this->cache->write('test', 'example'));
+		$this->assertSame('example', $this->cache->read('test'));
+		$this->assertTrue($this->cache->flush());
+		$this->assertFalse($this->cache->read('test'));
+	}
+}
