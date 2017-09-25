2688d6473a2726949e5074268cc796d743afb082
Alexey Kopytko <alexey@kopytko.com>
2017-09-25 16:27:55 +0900

    TagPaginateTest: missing tests for https

--- tests/Liquid/Tag/TagPaginateTest.php
+++ tests/Liquid/Tag/TagPaginateTest.php
@@ -21,15 +21,15 @@ class TagPaginateTest extends TestCase {
 	}
 
 	public function testVariables() {
-		$text = " {% paginate search.products by 3 %}{{ paginate.page_size }} {{ paginate.current_page }} {{ paginate.current_offset }} {{ paginate.pages }} {{ paginate.items }} {% endpaginate %}";
-		$expected = " 3 1 0 2 5 ";
+		$text = " {% paginate search.products by 3 %}{{ paginate.page_size }} {{ paginate.current_page }} {{ paginate.current_offset }} {{ paginate.pages }} {{ paginate.items }} {{ paginate.next.url }}{% endpaginate %}";
+		$expected = " 3 1 0 2 5 http://?page=2";
 		$this->assertTemplateResult($expected, $text, array('search' => array('products' => new \ArrayIterator(array(array('id' => 1), array('id' => 2), array('id' => 3), array('id' => 4), array('id' => 5))))));
 	}
 
 	public function testNextPage() {
-		$text = "{% paginate products by 1 %}{% for product in products %} {{ product.id }} {% endfor %}{% endpaginate %}";
-		$expected = " 2 ";
-		$this->assertTemplateResult($expected, $text, array('page' => 2,'products' => array(array('id' => 1), array('id' => 2), array('id' => 3), array('id' => 4), array('id' => 5))));
+		$text = '{% paginate products by 1 %}{% for product in products %} {{ product.id }} {% endfor %}<a href="{{ paginate.next.url }}">{{ paginate.next.title }}</a>{% endpaginate %}';
+		$expected = ' 2 <a href="https://example.com/products?page=3">Next</a>';
+		$this->assertTemplateResult($expected, $text, array('HTTP_HOST' => 'example.com', 'REQUEST_URI' => '/products', 'HTTPS' => 'on', 'page' => 2,'products' => array(array('id' => 1), array('id' => 2), array('id' => 3), array('id' => 4), array('id' => 5))));
 	}
 
 	/**
