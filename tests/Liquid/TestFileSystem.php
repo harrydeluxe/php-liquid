29753b35c331d43ce8a8179d085620fcce65060b
Alexey Kopytko <alexey@kopytko.com>
2017-09-25 11:43:24 +0900

    TestFileSystem: simplified test wrapper for 'Virtual' file system

---
+++ tests/Liquid/TestFileSystem.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is part of the Liquid package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package Liquid
+ */
+
+namespace Liquid;
+
+use Liquid\FileSystem\Virtual;
+
+class TestFileSystem extends Virtual
+{
+	/** @return TestFileSystem */
+	public static function fromArray($array)
+	{
+		return new static(function ($path) use ($array) {
+			if (isset($array[$path])) {
+				return $array[$path];
+			}
+
+			return '';
+		});
+	}
+}
