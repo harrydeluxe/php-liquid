c1dfbd596016075b2df5f01122bace55522178c6
Alexey Kopytko <alexey@kopytko.com>
2017-09-28 18:41:30 +0900

    CustomTagTest - for Template::registerTag

---
+++ tests/Liquid/CustomTagTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the Liquid package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package Liquid
+ */
+
+namespace Liquid;
+
+use Liquid\Tag\TagComment;
+
+class TagFoo extends TagComment
+{
+}
+
+class CustomTagTest extends TestCase
+{
+	/**
+	 * @expectedException \Liquid\LiquidException
+	 * @expectedExceptionMessage Unknown tag foo
+	 */
+	public function testUnknownTag()
+	{
+		$template = new Template();
+		$template->parse('[ba{% foo %} Comment {% endfoo %}r]');
+	}
+
+	public function testCustomTag()
+	{
+		$template = new Template();
+		$template->registerTag('foo', TagFoo::class);
+		$template->parse('[ba{% foo %} Comment {% endfoo %}r]');
+		$this->assertEquals('[bar]', $template->render());
+	}
+}
