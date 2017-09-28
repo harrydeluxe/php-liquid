391e152657f742cdd8995c7db612ab7cc805f540
Alexey Kopytko <alexey@kopytko.com>
2017-09-28 18:22:12 +0900

    TagExtends: try to keep the base templates in cache (that not extend anything)

--- tests/Liquid/Tag/TagExtendsTest.php
+++ tests/Liquid/Tag/TagExtendsTest.php
@@ -105,7 +105,8 @@ class TagExtendsTest extends TestCase
 		$template = new Template();
 		$template->setFileSystem(TestFileSystem::fromArray(array(
 			'outer' => "{% block content %}Outer{{ a }}{% endblock %}Spacer{{ a }}{% block footer %}Footer{{ a }}{% endblock %}",
-			'middle' => "{% extends 'outer' %}{% block content %}Inner{{ a }}{% endblock %}",
+			'middle' => "{% extends 'outer' %}{% block content %}Middle{{ a }}{% endblock %}",
+			'inner' => "{% extends 'middle' %}{% block content %}Inner{{ a }}{% endblock %}",
 		)));
 
 		$template->setCache(new Local());
@@ -113,7 +114,8 @@ class TagExtendsTest extends TestCase
 		$template->parseFile('outer')->render(['a' => '0']);
 		$template->parseFile('middle')->render(['a' => '1']);
 		$template->parseFile('middle')->render(['a' => '2']);
-		$this->assertEquals('Inner3Spacer3Footer3', $template->parseFile('middle')->render(['a' => '3']));
+		$this->assertEquals('Middle3Spacer3Footer3', $template->parseFile('middle')->render(['a' => '3']));
+		$this->assertEquals('Inner4Spacer4Footer4', $template->parseFile('inner')->render(['a' => '4']));
 	}
 
 	public function testCacheDiscardedIfFileChanges()
